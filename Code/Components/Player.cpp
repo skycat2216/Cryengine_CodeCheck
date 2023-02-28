e7b4f13b49b68aeae035203bae0f3297d659f10f
skycat2216 <33820902+skycat2216@users.noreply.github.com>
2023-03-01 00:35:34 +0800

    Pre-Baseline-2
    Func TryUpdateStance() only update tusCamOffset once and leave it

--- Code/Components/Player.cpp
+++ Code/Components/Player.cpp
@@ -22,17 +22,322 @@ namespace
     CRY_STATIC_AUTO_REGISTER_FUNCTION(&RegisterPlayerComponent);
 }
 
+CPlayerComponent::CPlayerComponent()
+    : m_pCameraComponent(nullptr)
+    , m_pInputComponent(nullptr)
+    , m_pAdvancedAnimationComponent(nullptr)
+    , m_pCharacterController(nullptr)
+
+    , vec2MouseDeltaRotation(ZERO)
+    , vec3CameraStandingPos(Vec3(0.f, 0.f, DEFAULT_CAMERA_HEIGHT_STANDING))
+    , vec3CameraCrouchPos(Vec3(0.f, 0.f, DEFAULT_CAMERA_HEIGHT_CROUCH))
+    , vec3CamEndOffset(Vec3(0.f,0.f,DEFAULT_CAMERA_HEIGHT_STANDING))
+    , quatCurrentYaw(IDENTITY)
+    , fCurrentPitch(0.f)
+
+    , m_currentPlayerState(DEFAULT_STATE)
+    , fMovementSpeed(ZERO)
+    , fWalkSpeed(DEFAULT_SPEED_WALKING)
+    , fSprintSpeed(DEFAULT_SPEED_RUNNING)
+    , fJumpHeight(DEFAULT_JUMP_ENERGY)    
+    , epsCurrentStance(DEFAULT_STANCE)
+    , epsDesireStance(DEFAULT_STANCE)
+    , fCapsuleHeightStanding(DEFAULT_CAPSULE_HEIGHT_STANDING)
+    , fCapsuleHeightCrouch(DEFAULT_CAPSULE_HEIGHT_CROUCHING)
+    , fCapsuleGroundOffset(DEFAULT_CAPSULE_GROUND_OFFSET)
+
+    , fRotationSpeed(DEFAULT_ROTATION_SPEED)
+    , fRotationLimitsMinPitch(DEFAULT_ROT_LIMIT_PITCH_MIN)
+    , fRotationLimitsMaxPitch(DEFAULT_ROT_LIMIT_PITCH_MAX)
+    {
+    }
+
+
+
 void CPlayerComponent::Initialize()
 {
     m_pCameraComponent = m_pEntity->GetOrCreateComponent<Cry::DefaultComponents::CCameraComponent>();
     m_pInputComponent = m_pEntity->GetOrCreateComponent<Cry::DefaultComponents::CInputComponent>();
     m_pCharacterController = m_pEntity->GetOrCreateComponent<Cry::DefaultComponents::CCharacterControllerComponent>();
     m_pAdvancedAnimationComponent = m_pEntity->GetOrCreateComponent<Cry::DefaultComponents::CAdvancedAnimationComponent>();
+
+
+
+    Reset();
+}
+
+void CPlayerComponent::InitializeInput()
+{
+
+    m_pInputComponent->RegisterAction("player", "moveforward", [this](int activatonMode, float value) {vec2MovementDelta.y = value; });
+    m_pInputComponent->BindAction("player", "moveforward", eAID_KeyboardMouse, eKI_W);
+
+
+    m_pInputComponent->RegisterAction("player", "movebackward", [this](int activatonMode, float value) {vec2MovementDelta.y = -value; });
+    m_pInputComponent->BindAction("player", "movebackward", eAID_KeyboardMouse, eKI_S);
+
+
+    m_pInputComponent->RegisterAction("player", "moveright", [this](int activatonMode, float value) {vec2MovementDelta.x = value; });
+    m_pInputComponent->BindAction("player", "moveright", eAID_KeyboardMouse, eKI_D);
+
+
+    m_pInputComponent->RegisterAction("player", "moveleft", [this](int activatonMode, float value) {vec2MovementDelta.x = -value; });
+    m_pInputComponent->BindAction("player", "moveleft", eAID_KeyboardMouse, eKI_A);
+
+
+    m_pInputComponent->RegisterAction("player", "sprint", [this](int activationMode, float value)
+        {
+            if (activationMode == (int)eAAM_OnPress)
+            {
+                m_currentPlayerState = EPlayerState::Sprinting;
+            }
+            else if (activationMode == eAAM_OnRelease)
+            {
+                m_currentPlayerState = EPlayerState::Walking;
+            }
+        });
+    m_pInputComponent->BindAction("player", "sprint", eAID_KeyboardMouse, eKI_LCtrl);
+
+
+    m_pInputComponent->RegisterAction("player", "canter", [this](int activationMode, float value)
+        {
+            if (activationMode == (int)eAAM_OnPress)
+            {
+                m_currentPlayerState = EPlayerState::Canter;
+            }
+            else if (activationMode == eAAM_OnRelease)
+            {
+                m_currentPlayerState = EPlayerState::Walking;
+            }
+        });
+    m_pInputComponent->BindAction("player", "canter", eAID_KeyboardMouse, eKI_LAlt);
+
+
+    m_pInputComponent->RegisterAction("player", "jump", [this](int activationMode, float value)
+    {
+        if (m_pCharacterController->IsOnGround())
+        {
+                iJumpCount = 0;
+        }
+        if (activationMode == (int)eAAM_OnPress && iJumpCount < iMaxJump)
+        {
+            CryLog("A Jump Call");
+            m_pCharacterController->AddVelocity(Vec3(0, 0, fJumpHeight));
+            iJumpCount++;
+        }
+    });
+    m_pInputComponent->BindAction("player", "jump", eAID_KeyboardMouse, eKI_Space);
+
+
+    m_pInputComponent->RegisterAction("player", "crouch", [this](int activationMode, float value)
+    {
+        if (activationMode == (int)eAAM_OnPress)
+        {
+            epsDesireStance = EPlayerStance::Crouch;
+            CryLog("Crouch pressed");
+        }
+        else if (activationMode == (int)eAAM_OnRelease)
+        {
+            epsDesireStance = EPlayerStance::Standing;
+        }
+    });
+    m_pInputComponent->BindAction("player", "crouch", eAID_KeyboardMouse, eKI_LShift);
+
+
+    m_pInputComponent->RegisterAction("player", "yaw", [this](int activatonMode, float value) {vec2MouseDeltaRotation.x = -value; });
+    m_pInputComponent->BindAction("player", "yaw", eAID_KeyboardMouse, eKI_MouseX);
+
+
+    m_pInputComponent->RegisterAction("player", "pitch", [this](int iActivatonMode, float value) {vec2MouseDeltaRotation.y = -value; });
+    m_pInputComponent->BindAction("player", "pitch", eAID_KeyboardMouse, eKI_MouseY);
+
+    m_pInputComponent->RegisterAction("player", "camswitch", [this](int iActivationMode, float value)
+        {
+            /*
+            if (iActivationMode == eIS_Pressed)
+            {
+
+                if (is_FPS)
+                {
+                    if(playerState == ePS_Crouching ||)
+                        CryLog("CamChange to FPS Detected");
+                    is_FPS = false;
+                }
+            }
+            else
+            {
+                CryLog("CamChange to TPS Detected");
+                is_FPS = true;
+            }
+            */
+        });
+    m_pInputComponent->BindAction("player", "camswitch", eAID_KeyboardMouse, eKI_F2);
+
+}
+
+void CPlayerComponent::Reset()
+{
+    vec2MovementDelta = ZERO;
+    vec2MouseDeltaRotation = ZERO;
+
+    InitializeInput();
+
+    m_currentPlayerState = EPlayerState::Walking;
+    epsCurrentStance = EPlayerStance::Standing;
+    epsDesireStance = epsCurrentStance;
+
+
+    quatCurrentYaw = Quat::CreateRotationZ(m_pEntity->GetWorldRotation().GetRotZ());
+    fCurrentPitch = 0.f;
+
+    vec3CamEndOffset = vec3CameraStandingPos;
+
+}
+
+
+
+void CPlayerComponent::RecenterCollider()
+{
+    static bool skip = false;
+    if (skip)
+    {
+        skip = false;
+        return;
+    }
+
+    auto pChacterController = m_pEntity->GetComponent<Cry::DefaultComponents::CCharacterControllerComponent>();
+    if (pChacterController == nullptr)
+    {
+        return;
+    }
+
+    const auto& physParams = m_pCharacterController->GetPhysicsParameters();
+    float heightOffset = physParams.m_height*0.5f;
+    if (physParams.m_bCapsule)
+    {
+        heightOffset = heightOffset * 0.5f + physParams.m_radius * 0.5f;
+    }
+
+    m_pCharacterController->SetTransformMatrix(Matrix34((IDENTITY), Vec3(0.f, 0.f, 0.005f + heightOffset)));
+
+    skip = true;
+
+    m_pCharacterController->Physicalize();
+}
+
+void CPlayerComponent::UpdateMovement()
+{
+
+    Vec3 velocity = Vec3(vec2MovementDelta.x, vec2MovementDelta.y, 0.0f);
+    velocity.Normalize();
+    fMovementSpeed = m_currentPlayerState == EPlayerState::Sprinting ? fSprintSpeed : fWalkSpeed;
+    //m_pEntity->SetPos(m_pEntity->GetWorldPos() + Vec3(vec2MovementDelta.x, vec2MovementDelta.y, 0.0f));
+    m_pCharacterController->SetVelocity(m_pEntity->GetWorldRotation() * velocity * fMovementSpeed);
+}
+
+void CPlayerComponent::UpdateRotation()
+{
+    quatCurrentYaw *= Quat::CreateRotationZ(vec2MouseDeltaRotation.x * fRotationSpeed);
+    m_pEntity->SetRotation(quatCurrentYaw);
+}
+
+void CPlayerComponent::UpdateCamera(float fFrametime)
+{
+    fCurrentPitch = crymath::clamp(fCurrentPitch + vec2MouseDeltaRotation.y * fRotationSpeed, fRotationLimitsMinPitch, fRotationLimitsMaxPitch);
+
+    Vec3 vec3CurrentCamOffset = m_pCameraComponent->GetTransformMatrix().GetTranslation();
+    vec3CurrentCamOffset = Vec3::CreateLerp(vec3CurrentCamOffset, vec3CamEndOffset, 10.0f * fFrametime);
+
+    Matrix34 finalCamMartix;
+    finalCamMartix.SetTranslation(vec3CurrentCamOffset);
+    finalCamMartix.SetRotation33(Matrix33::CreateRotationX(fCurrentPitch));
+    m_pCameraComponent->SetTransformMatrix(finalCamMartix);
+}
+
+void CPlayerComponent::TryUpdateStance()
+{
+    if (epsDesireStance == epsCurrentStance) return;
+    IPhysicalEntity* pPhysEnt = m_pEntity->GetPhysicalEntity();
+    if (pPhysEnt == nullptr)
+    {
+        CryLog("EPS Nullptr detected");
+        return;
+    }
+
+    const float radius = m_pCharacterController->GetPhysicsParameters().m_radius * 0.5f;
+
+    float tusHeight = 0.f;
+    Vec3 tusCamOffset = ZERO;
+
+    switch (epsDesireStance)
+    {
+        case EPlayerStance::Ground:
+        {
+            CryLog("No Actual Implement");
+        }break;
+
+        case EPlayerStance::Crouch:
+        {
+            tusHeight = fCapsuleHeightCrouch;
+            tusCamOffset = vec3CameraCrouchPos;
+        }break;
+
+        case EPlayerStance::Standing:
+        {
+            tusHeight = fCapsuleHeightStanding;
+            tusCamOffset = vec3CameraStandingPos;
+            CryLog("Standing confrimed");
+            primitives::capsule capsol;
+
+            capsol.axis.Set(0, 0, 1);
+
+            capsol.center = m_pEntity->GetWorldPos() + Vec3(0, 0, fCapsuleGroundOffset + radius + tusHeight * 0.5f);
+            capsol.r = radius;
+            capsol.hh = tusHeight * 0.5f;
+
+            if (IsCapsuleIntersectingGeometry(capsol)) return;
+
+
+        }break;
+    }
+    pe_player_dimensions playerDemensions;
+    pPhysEnt->GetParams(&playerDemensions);
+
+    playerDemensions.heightCollider = fCapsuleGroundOffset + radius + tusHeight * 0.5f;
+    playerDemensions.sizeCollider = Vec3(radius, radius, tusHeight * 0.5f);
+    vec3CamEndOffset = tusCamOffset;
+
+    epsCurrentStance = epsDesireStance;
+
+    pPhysEnt->SetParams(&playerDemensions);
+}
+
+bool CPlayerComponent::IsCapsuleIntersectingGeometry(const primitives::capsule& capsule) const
+{
+    IPhysicalEntity* pPhysEnt = m_pEntity->GetPhysicalEntity();
+
+    if (pPhysEnt == nullptr)
+        return false;
+
+    IPhysicalWorld::SPWIParams pwiParams;
+
+    pwiParams.itype = capsule.type;
+    pwiParams.pprim = &capsule;
+
+    pwiParams.pSkipEnts = &pPhysEnt;
+    pwiParams.nSkipEnts = 1;
+
+    intersection_params intersectionParams;
+    intersectionParams.bSweepTest = false;
+    pwiParams.pip = &intersectionParams;
+
+    const int contactCount = static_cast<int>(gEnv->pPhysicalWorld -> PrimitiveWorldIntersection(pwiParams));
+    return contactCount > 0;
 }
 
 Cry::Entity::EventFlags CPlayerComponent::GetEventMask() const
 {
-    return Cry::Entity::EEvent::GameplayStarted | Cry::Entity::EEvent::Update | Cry::Entity::EEvent::Reset;
+    return Cry::Entity::EEvent::GameplayStarted | Cry::Entity::EEvent::Update | Cry::Entity::EEvent::PhysicalTypeChanged | Cry::Entity::EEvent::Reset;
 }
 
 void CPlayerComponent::ProcessEvent(const SEntityEvent& event)
@@ -43,22 +348,25 @@ void CPlayerComponent::ProcessEvent(const SEntityEvent& event)
         case Cry::Entity::EEvent::GameplayStarted:
         {
         
-            InitializeInput();
+            Reset();
 
         }break;
 
 
         case Cry::Entity::EEvent::Update:
         {
+            const float fFrametime = event.fParam[0];
+
+            TryUpdateStance();
+            UpdateMovement();
+            UpdateRotation();
+            UpdateCamera(fFrametime);
 
 
-            Vec3 velocity = Vec3(vec2MovementDelta.x, vec2MovementDelta.y, 0.0f);
-            velocity.Normalize();
-            fMovementSpeed = m_currentPlayerState == EPlayerState::Sprinting?fSprintSpeed:fWalkSpeed;
-            //m_pEntity->SetPos(m_pEntity->GetWorldPos() + Vec3(vec2MovementDelta.x, vec2MovementDelta.y, 0.0f));
-            m_pCharacterController->SetVelocity(m_pEntity->GetWorldRotation() * velocity * fMovementSpeed);
 
 
+
+            /*
             Ang3 rotationAng = CCamera::CreateAnglesYPR(Matrix33(quatLookOrientation));
             rotationAng.x += vec2MouseDeltaRotation.x*fRotationSpeed;
             rotationAng.y = CLAMP(rotationAng.y +  vec2MouseDeltaRotation.y * fRotationSpeed, fRotationLimitsMinPitch, fRotationLimitsMaxPitch);
@@ -80,126 +388,22 @@ void CPlayerComponent::ProcessEvent(const SEntityEvent& event)
             finalCamMartix.SetTranslation(m_pCameraComponent->GetTransformMatrix().GetTranslation());
             finalCamMartix.SetRotation33(camRotation);
             m_pCameraComponent->SetTransformMatrix(finalCamMartix);
-
+            */
             
 
         }break;
+        case Cry::Entity::EEvent::PhysicalTypeChanged:
+        {
+            RecenterCollider();
+        }break;
         case Cry::Entity::EEvent::Reset:
         {
-            vec2MovementDelta = ZERO;
-            vec2MouseDeltaRotation = ZERO;
-            quatLookOrientation = IDENTITY;
+            Reset();
+        }break;
 
-            
-            //I still can't believe it disappar after I hit ctrl+c and ctrl+v the vec3CameraPos in the code below.
-            //F
-            Matrix34 camDefaultMatrix;
-            camDefaultMatrix.SetTranslation(vec3CameraDefaultPos);
-            camDefaultMatrix.SetRotation33(Matrix33(m_pEntity->GetWorldRotation()));
-            m_pCameraComponent->SetTransformMatrix(camDefaultMatrix);
-            break;
-            
-        }
     }
 }
-void CPlayerComponent::InitializeInput()
-{
 
-    m_pInputComponent->RegisterAction("player", "moveforward", [this](int activatonMode, float value) {vec2MovementDelta.y = value; });
-    m_pInputComponent->BindAction("player", "moveforward", eAID_KeyboardMouse, eKI_W);
-
-
-    m_pInputComponent->RegisterAction("player", "movebackward", [this](int activatonMode, float value) {vec2MovementDelta.y = -value; });
-    m_pInputComponent->BindAction("player", "movebackward", eAID_KeyboardMouse, eKI_S);
-
-
-    m_pInputComponent->RegisterAction("player", "moveright", [this](int activatonMode, float value) {vec2MovementDelta.x = value; });
-    m_pInputComponent->BindAction("player", "moveright", eAID_KeyboardMouse, eKI_D);
-
-
-    m_pInputComponent->RegisterAction("player", "moveleft", [this](int activatonMode, float value) {vec2MovementDelta.x = -value; });
-    m_pInputComponent->BindAction("player", "moveleft", eAID_KeyboardMouse, eKI_A);
-
-
-    m_pInputComponent->RegisterAction("player", "sprint", [this](int activationMode, float value) 
-    {
-            if (activationMode == (int)eAAM_OnPress)
-            {
-                m_currentPlayerState = EPlayerState::Sprinting;
-            }
-            else if (activationMode == eAAM_OnRelease)
-            {
-                m_currentPlayerState = EPlayerState::Walking;
-            }
-    });
-    m_pInputComponent->BindAction("player", "sprint", eAID_KeyboardMouse, eKI_LCtrl);
-
-
-
-
-
-    m_pInputComponent->RegisterAction("player", "jump", [this](int activationMode, float value)
-        {
-
-            if (m_pCharacterController->IsOnGround())
-            {
-                iJumpCount=0;
-            }
-           if(activationMode == (int)eAAM_OnPress && iJumpCount < iMaxJump)
-            {
-                CryLog("A Jump Call");
-                m_pCharacterController->AddVelocity(Vec3(0, 0, fJumpHeight));
-                iJumpCount ++;
-            }
-        });
-    m_pInputComponent->BindAction("player", "jump", eAID_KeyboardMouse, eKI_Space);
-
-        //old
-        /*
-        m_pInputComponent->RegisterAction("player", "jump", [this](int iActivationMode, float value)
-        {
-                vec2MovementDelta.z = value;
-        });
-        m_pInputComponent->BindAction("player", "jump", eAID_KeyboardMouse, eKI_Space);
-
-
-        m_pInputComponent->RegisterAction("player", "crouch", [this](int iActivationMode, float value)
-        {
-            vec2MovementDelta.z = -value;
-        });
-        m_pInputComponent->BindAction("player", "crouch", eAID_KeyboardMouse, eKI_LShift);
-        */
-
-    m_pInputComponent->RegisterAction("player", "yaw", [this](int activatonMode, float value) {vec2MouseDeltaRotation.x = -value; });
-    m_pInputComponent->BindAction("player", "yaw", eAID_KeyboardMouse, eKI_MouseX);
-
-
-    m_pInputComponent->RegisterAction("player", "pitch", [this](int iActivatonMode, float value) {vec2MouseDeltaRotation.y = -value; });
-    m_pInputComponent->BindAction("player", "pitch", eAID_KeyboardMouse, eKI_MouseY);
-    
-    m_pInputComponent->RegisterAction("player", "camswitch", [this](int iActivationMode, float value)
-        {
-            /*
-            if (iActivationMode == eIS_Pressed)
-            {
-                
-                if (is_FPS)
-                {
-                    if(playerState == ePS_Crouching ||)
-                        CryLog("CamChange to FPS Detected");
-                    is_FPS = false;
-                }
-            }
-            else
-            {
-                CryLog("CamChange to TPS Detected");
-                is_FPS = true;
-            }
-            */
-        });
-        m_pInputComponent->BindAction("player", "camswitch", eAID_KeyboardMouse, eKI_F2);
-        
-    }
 
 
 
--- Code/Components/Player.h
+++ Code/Components/Player.h
@@ -25,39 +25,60 @@ namespace Cry::DefaultComponents
 	class CAdvancedAnimationComponent;
 }
 
+namespace primitives
+{
+	struct capsule;
+}
+
 ////////////////////////////////////////////////////////
 // Represents a player participating in gameplay
 ////////////////////////////////////////////////////////
 
 enum class EPlayerState
 {
-	Walking, Canter, Sprinting
+	Walking, 
+	Canter, 
+	Sprinting
 };
+
+enum class EPlayerStance
+{
+	Ground,
+	Crouch,
+	Standing
+};
+
+//Deceased
 
 class CPlayerComponent final : public IEntityComponent
 {
 public:
-	CPlayerComponent() = default;
-	virtual ~CPlayerComponent() = default;
+	CPlayerComponent();
+	virtual ~CPlayerComponent() override {};
 
 	// Reflect type to set a unique identifier for this component
 	static void ReflectType(Schematyc::CTypeDesc<CPlayerComponent>& desc)
 	{
 		desc.SetGUID("{63F4C0C6-32AF-4ACB-8FB0-57D45DD14725}"_cry_guid);
-		desc.AddMember(&CPlayerComponent::fWalkSpeed, 'pms', "playerwalkspeed", "Player Walk Speed", "Set walk speed of player", ZERO);
-		desc.AddMember(&CPlayerComponent::fCanterSpeed, 'fcs', "playercanterspeed", "Player Canter Speed", "Set canter speed of player", ZERO);
-		desc.AddMember(&CPlayerComponent::fSprintSpeed, 'frs', "playersprintspeed", "Player Run Speed", "Set Move Speed Of Player", ZERO);
-		desc.AddMember(&CPlayerComponent::fRotationSpeed,'pros', "playerrotationspeed", "Player Rotation Speed", "Sets the speed of the players rotation", ZERO);
-		desc.AddMember(&CPlayerComponent::fJumpHeight, 'fjh', "playerjumphheight", "Player Jump Height", "Sets the jump height (or initial velocity?) of player",1.0f);
-		desc.AddMember(&CPlayerComponent::iMaxJump, 'fmj', "playermaxjump", "Player Max Jump Count", "Sets how many times player can jump",1);
+		desc.AddMember(&CPlayerComponent::fWalkSpeed, 'pms', "playerwalkspeed", "Player Walk Speed", "Set walk speed of player", DEFAULT_SPEED_WALKING);
+		desc.AddMember(&CPlayerComponent::fCanterSpeed, 'fcs', "playercanterspeed", "Player Canter Speed", "Set canter speed of player", DEFAULT_SPEED_CANTER);
+		desc.AddMember(&CPlayerComponent::fSprintSpeed, 'frs', "playersprintspeed", "Player Run Speed", "Set Move Speed Of Player", DEFAULT_SPEED_RUNNING);
+		desc.AddMember(&CPlayerComponent::fRotationSpeed,'pros', "playerrotationspeed", "Player Rotation Speed", "Sets the speed of the players rotation", DEFAULT_ROTATION_SPEED);
+		desc.AddMember(&CPlayerComponent::fJumpHeight, 'fjh', "playerjumphheight", "Player Jump Height", "Sets the jump height (or initial velocity?) of player", DEFAULT_JUMP_ENERGY);
+		desc.AddMember(&CPlayerComponent::iMaxJump, 'fmj', "playermaxjump", "Player Max Jump Count", "Sets how many times player can jump", DEFAULT_JUMP_FREQUENCY);
 
-		desc.AddMember(&CPlayerComponent::vec3CameraDefaultPos, 'cdp', "cameradefaultpos", "Camera Default Position", "Sets camera default position", ZERO);
+		desc.AddMember(&CPlayerComponent::vec3CameraStandingPos, 'csp', "camerastandingpos", "Camera Standing Position", "Sets standing camera default position", ZERO);
+		desc.AddMember(&CPlayerComponent::vec3CameraCrouchPos, 'ccp', "cameracrouchpos", "Camera Crouch Position", "Sets crouch camera default position", ZERO);
 		desc.AddMember(&CPlayerComponent::fRotationLimitsMaxPitch, 'cpm', "camerapitchmax", "Camera Pitch Max", "Maximum rotation value for camera pitch", 1.5f);
 		desc.AddMember(&CPlayerComponent::fRotationLimitsMinPitch, 'cpmi', "camerapitchmin", "Camera Pitch Min", "Minimum rotation value for camera pitch", -0.85f);
 
+		desc.AddMember(&CPlayerComponent::fCapsuleHeightCrouch, 'capc', "capsuleheightcrouching", "Capsule Crouching Height", "Height of collision capsule while crouching", DEFAULT_CAPSULE_HEIGHT_CROUCHING);
+		desc.AddMember(&CPlayerComponent::fCapsuleHeightStanding, 'caps', "capsuleheightstanding", "Capsule Standing Height", "Height of collision capsule while standing", DEFAULT_CAPSULE_HEIGHT_STANDING);
+		desc.AddMember(&CPlayerComponent::fCapsuleGroundOffset, 'capo', "capsulegroundoffset", "Capsule Ground Offset", "Offset of the capsule from the entity floor", DEFAULT_CAPSULE_GROUND_OFFSET);
+
 		desc.AddMember(&CPlayerComponent::fCamOffsetTPSForward, 'cof', "cameraoffsetforward", "Camera Offset Forward", "IDK#1",ZERO);
 		desc.AddMember(&CPlayerComponent::fCamOffsetTPSUp, 'cou', "cameraoffsetup", "Camera Offset Upward", "IDK#2", ZERO);
-		desc.AddMember(&CPlayerComponent::fCamOffsetFPSForward, 'cof', "cameraoffsetforward", "Camera Offset Forward", "IDK#1", ZERO);
+		desc.AddMember(&CPlayerComponent::fCamOffsetFPSForward, 'cof', "cameraoffsetforward", "Camera Offset Forward", "IDK#2", ZERO);
 		desc.AddMember(&CPlayerComponent::fCamOffsetFPSUp, 'cou', "cameraoffsetup", "Camera Offset Upward", "IDK#2", ZERO);
 
 	}
@@ -66,42 +87,92 @@ public:
 	virtual Cry::Entity::EventFlags GetEventMask() const override;
 	virtual void ProcessEvent(const SEntityEvent& event) override;
 
+
+protected:
+	void Reset();
+	void InitializeInput();
+
+	void RecenterCollider();
+	void UpdateMovement();
+	void UpdateRotation();
+	void UpdateCamera(float fFrametime);
+	void TryUpdateStance();
+	bool IsCapsuleIntersectingGeometry(const primitives::capsule& capsule) const;
+
 private:
 	Cry::DefaultComponents::CCameraComponent* m_pCameraComponent;
 	Cry::DefaultComponents::CInputComponent* m_pInputComponent;
 	Cry::DefaultComponents::CCharacterControllerComponent* m_pCharacterController;
 	Cry::DefaultComponents::CAdvancedAnimationComponent* m_pAdvancedAnimationComponent;
 
-	void InitializeInput();
-	void UpdateCameraPos();
+	static constexpr EPlayerState DEFAULT_STATE = EPlayerState::Walking;
+	static constexpr float DEFAULT_SPEED_WALKING = 3;
+	static constexpr float DEFAULT_SPEED_CANTER = 6;
+	static constexpr float DEFAULT_SPEED_RUNNING = 9;
+	static constexpr float DEFAULT_JUMP_ENERGY = 3;
+	static constexpr float DEFAULT_JUMP_FREQUENCY = 2;
+	static constexpr float DEFAULT_CAPSULE_HEIGHT_CROUCHING = 0.8;
+	static constexpr float DEFAULT_CAPSULE_HEIGHT_STANDING = 1.6;
+	static constexpr float DEFAULT_CAPSULE_GROUND_OFFSET = 0.2;
+
+	static constexpr EPlayerStance DEFAULT_STANCE = EPlayerStance::Standing;
+
+	static constexpr float DEFAULT_CAMERA_HEIGHT_STANDING = 2.2;
+	static constexpr float DEFAULT_CAMERA_HEIGHT_CROUCH = 3.0;
+
+	static constexpr float DEFAULT_ROTATION_SPEED = 0.002;
+	static constexpr float DEFAULT_ROT_LIMIT_PITCH_MIN = -0.9;
+	static constexpr float DEFAULT_ROT_LIMIT_PITCH_MAX = 1.15;
+
 
 	
 
-	Vec2 vec2MouseDeltaRotation;
-	Vec2 vec2MovementDelta;
-	Vec3 vec3CameraDefaultPos;
-	
-	Quat quatLookOrientation;
+	//Vars of camera 
+	Vec3 vec3CameraStandingPos;
+	Vec3 vec3CameraCrouchPos;
+	Vec3 vec3CameraFinalPos;
 
-	float fMovementSpeed;
-	float fWalkSpeed;
-	float fCanterSpeed;
-	float fSprintSpeed;
-	float fRotationSpeed;
-	float fRotationLimitsMinPitch;
-	float fRotationLimitsMaxPitch;
-	float fJumpHeight;
-	int iMaxJump;
-	int iJumpCount;
-
+	Vec3 vec3CamEndOffset;
+	Quat quatCurrentYaw;
+	float fCurrentPitch;
 
 	float fCamOffsetTPSForward;
 	float fCamOffsetTPSUp;
 	float fCamOffsetFPSForward;
 	float fCamOffsetFPSUp;
-
-	EPlayerState m_currentPlayerState;
-	
 	bool is_FPS = false;
+
+	Quat quatLookOrientation;
+
+
+	//Vars of Player
+	EPlayerState m_currentPlayerState;
+
+	Vec2 vec2MovementDelta;
+	float fMovementSpeed;
+	float fCrouchSpeed;
+	float fWalkSpeed;
+	float fCanterSpeed;
+	float fSprintSpeed;
+
+	float fJumpHeight;
+	int iMaxJump;
+	int iJumpCount;
+
+	EPlayerStance epsCurrentStance;
+	EPlayerStance epsDesireStance;
+	float fCapsuleHeightStanding;
+	float fCapsuleHeightCrouch;
+	float fCapsuleGroundOffset;
+
+
+	//Vars of mouse rotation
+
+	Vec2 vec2MouseDeltaRotation;
+	float fRotationSpeed;
+	float fRotationLimitsMinPitch;
+	float fRotationLimitsMaxPitch;
+	
+
 	
 };
